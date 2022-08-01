98abc9c48b1b8a5d60d31c36f48440b86c5b014e
krlove <mischenkoandrey@gmail.com>
2022-08-01 20:21:50 +0200

    Test Map::count

--- tests/Map/MapTest.php
+++ tests/Map/MapTest.php
@@ -47,4 +47,28 @@ class MapTest extends TestCase
         self::assertCount(1, $copy);
         self::assertSame($value1, $copy->get($key1));
     }
+
+    /**
+     * @dataProvider keyValueTypesDataProvider
+     */
+    public function testCount(string $keyType, string $valueType, $key1, $value1, $key2, $value2): void
+    {
+        $map = Map::of($keyType, $valueType);
+        self::assertEquals(0, $map->count());
+        $map->set($key1, $value1);
+        self::assertCount(1, $map);
+
+        if ($keyType === 'null') {
+            return;
+        }
+
+        $map->set($key2, $value1);
+        self::assertCount(2, $map);
+        $map->set($key2, $value2);
+        self::assertCount(2, $map);
+        $map->remove($key1);
+        self::assertCount(1, $map);
+        $map->remove($key2);
+        self::assertCount(0, $map);
+    }
 }
