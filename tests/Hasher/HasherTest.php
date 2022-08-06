f8fdaccaeaa42def1a0e2ee54da9708fb2018050
krlove <mischenkoandrey@gmail.com>
2022-08-06 14:53:13 +0200

    Test hasher

---
+++ tests/Hasher/HasherTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Krlove\Hasher;
+
+use Krlove\Collection\Hasher\Hasher;
+use PHPUnit\Framework\TestCase;
+use Tests\Krlove\Stub\Obj1;
+use Tests\Krlove\Stub\Obj2;
+
+class HasherTest extends TestCase
+{
+    public function testHashUnique(): void
+    {
+        $r1 = fopen(__DIR__ . '/../resources/test.txt', 'r');
+        $r2 = fopen(__DIR__ . '/../resources/test.txt', 'r+');
+        $r3 = fopen(__DIR__ . '/../resources/test.txt', 'w+');
+        fclose($r3);
+
+        $values = [
+            null,
+            true,
+            false,
+            0,
+            100,
+            -100,
+            PHP_INT_MIN,
+            PHP_INT_MAX,
+            0.0,
+            100.0,
+            -100.0,
+            PHP_FLOAT_MIN,
+            PHP_FLOAT_MAX,
+            '',
+            '0',
+            '100',
+            '-100',
+            [],
+            ['foo'],
+            ['bar'],
+            ['foo' => 'bar'],
+            function () {},
+            function () {},
+            function () { return true; },
+            $r1,
+            $r2,
+            $r3,
+            new Obj1(1),
+            new Obj1(1),
+            new Obj1(2),
+            new Obj2(3),
+        ];
+
+        $hashedValues = [];
+        foreach ($values as $value) {
+            $hashedValue = Hasher::hash($value);
+            $hashedValues[$hashedValue] = true;
+        }
+
+        self::assertSameSize($values, $hashedValues);
+        fclose($r1);
+        fclose($r2);
+    }
+}
